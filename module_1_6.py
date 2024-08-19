b3215cb610fb809e2513a13deda62a8736550aa7
Олег <belun-oleg@yandex.ru>
2024-08-19 22:44:51 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,11 @@
+my_dick = {'Oleg': 1985, 'Polena': 2008, 'Kosty': 2017, 'Natasha': 1987}
+print(my_dick)
+my_dick = {'Oleg': 1985, 'Polena': 2008, 'Kosty': 2017, 'Natasha': 1987}
+print(my_dick['Kosty'])
+my_dick = {'Oleg': 1985, 'Polena': 2008, 'Kosty': 2024, 'Natasha': 1987}
+print(my_dick.get('Kosty'))
+my_dick.update({'Elena': 1964,
+                'Svetlana': 1954})
+print(my_dick)
+del my_dick['Elena']
+print(my_dick)
